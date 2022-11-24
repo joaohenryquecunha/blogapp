2e49dd28eb7571d2bed7e30192bcaff52ea39c5a
joao henryque <henryque_joao17@hotmail.com>
2022-11-24 00:44:31 -0300

    inicio do projeto

---
+++ app.js
@@ -0,0 +1,27 @@
+// Carregando módulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+    const admin = require("./routes/admin")
+    const path = require("path")
+    //const mongoose = require('mongoose')
+
+// Configurações
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // Handlebars
+        app.engine('handlebars',handlebars.engine({default: 'main'}))
+        app.set('view engine', 'handlebars')  
+    // Mongoose  
+        //em brave... 
+    // Public
+        app.use(express.static(path.join(__dirname,"public")))     
+// Rotas
+        app.use('/admin', admin)
+// Outros
+const PORT = 27017
+app.listen(PORT,() => {
+    console.log("Servidor rodando! ")
+})
