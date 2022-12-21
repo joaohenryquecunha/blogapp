94d1026bd3f01ed39719af8e47d3364faf731324
joao henryque <henryque_joao17@hotmail.com>
2022-12-21 17:49:17 -0300

    vercel

--- app.js
+++ app.js
@@ -121,5 +121,5 @@ app.get('/posts', (req, res) => {
 // Outros
 const PORT = process.env.PORT || 27017
 app.listen(PORT,() => {
-    console.log("Servidor rodando! ")
+    console.log("servidor rodando na "+(PORT))
 })
